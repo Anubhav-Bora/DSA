999e1b258470b06f18685b4cd829856bc2b631da
Anubhav <anubhavbora40@gmail.com>
2025-08-26 21:53:02 +0530

    N-ary

---
+++ Binary Tree/N-Binary Tree/Maximum Depth of N-ary Tree.cpp
@@ -0,0 +1,55 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+// Definition for a Node.
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+class Solution
+{
+public:
+    int LOT(Node *root)
+    {
+        if (!root)
+            return 0;
+        queue<Node *> q;
+        q.push(root);
+        int count = 0;
+        while (!q.empty())
+        {
+            int size = q.size();
+            for (int i = 0; i < size; i++)
+            {
+                Node *curr = q.front();
+                q.pop();
+                for (Node *child : curr->children)
+                {
+                    q.push(child);
+                }
+            }
+            count++;
+        }
+        return count;
+    }
+    int maxDepth(Node *root)
+    {
+        return LOT(root);
+    }
+};
