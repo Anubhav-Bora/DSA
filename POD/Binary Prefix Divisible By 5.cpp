f6a97fd31acdd5ebe8c2368b5accb429431c98a4
Anubhav <anubhavbora40@gmail.com>
2025-11-24 22:07:11 +0530

    BINARY-DECIMAL

---
+++ POD/Binary Prefix Divisible By 5.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    vector<bool> prefixesDivBy5(vector<int> &nums)
+    {
+        vector<bool> ans;
+        int value = 0;
+
+        for (int bit : nums)
+        {
+            value = (value * 2 + bit) % 5; // main logic
+            ans.push_back(value == 0);
+        }
+
+        return ans;
+    }
+};
