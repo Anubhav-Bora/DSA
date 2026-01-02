242fea69221cdf332558a187146776b42daceb4b
Anubhav <anubhavbora40@gmail.com>
2026-01-02 21:56:26 +0530

    solve

--- DP/DP  on Grid/Maximum Difference Score in a Grid.cpp
+++ DP/DP  on Grid/Maximum Difference Score in a Grid.cpp
@@ -1,32 +1,49 @@
-#include <bits/stdc++.h>
-using namespace std;
 class Solution
 {
 public:
-    int maxScore(vector<vector<int>> &grid)
+    int solve(int i, int j, vector<vector<int>> &grid, vector<vector<int>> &dp)
     {
-        int ans = INT_MIN;
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n));
-        dp[m - 1][n - 1] = grid[m - 1][n - 1];
-        for (int i = m - 1; i >= 0; i--)
+
+        if (i >= m || j >= n)
+            return INT_MIN;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        int best = INT_MIN;
+
+        if (i + 1 < m)
+            best = max(best, grid[i + 1][j]);
+
+        if (j + 1 < n)
+            best = max(best, grid[i][j + 1]);
+
+        int down = (i + 1 < m) ? solve(i + 1, j, grid, dp) : INT_MIN;
+        int right = (j + 1 < n) ? solve(i, j + 1, grid, dp) : INT_MIN;
+
+        best = max(best, max(down, right));
+
+        dp[i][j] = best;
+        return best;
+    }
+
+    int maxScore(vector<vector<int>> &grid)
+    {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+
+        int ans = INT_MIN;
+        for (int i = 0; i < m; i++)
         {
-            for (int j = n - 1; j >= 0; j--)
+            for (int j = 0; j < n; j++)
             {
-                if (i == m - 1 && j == n - 1)
-                    continue;
-                int maxi = INT_MIN;
-                if (i + 1 < m)
-                {
-                    maxi = max(maxi, dp[i + 1][j]);
-                }
-                if (j + 1 < n)
-                {
-                    maxi = max(maxi, dp[i][j + 1]);
-                }
-                ans = max(ans, maxi - grid[i][j]);
-                dp[i][j] = max(maxi, grid[i][j]);
+                int futureMax = solve(i, j, grid, dp);
+                if (futureMax != INT_MIN)
+                    ans = max(ans, futureMax - grid[i][j]);
             }
         }
         return ans;
